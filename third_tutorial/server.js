a9bc708712f4308d14ddf8ebdeba9050ad1c488e
unknown <nikimalmsten@hotmail.com>
2024-11-22 14:40:13 +0200

    updating files

--- third_tutorial/server.js
+++ third_tutorial/server.js
@@ -1,40 +1,28 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import exp from 'constants';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFfound.js';
 const app = express();
 const port = process.env.PORT || 7000;
 
+// Body parser middelware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Logger middleware
+app.use(logger);
+
 // Setup static folder
-//app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-    {id: 1, title: 'Post one'},
-    {id: 2, title: 'Post two'},
-    {id: 3, title: 'Post tree'}
-];
+// Routes
+app.use('/api/posts', posts);
+app.use(notFound)
 
-// Get all posts
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        res.status(200).json(posts.slice(0, limit));
-    }
-    else {
-        res.json(posts);
-    }
-    res.status(200).json(posts);
-});
+// Error handelr
+app.use(errorHandler);
 
-// Get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if(!post) {
-        res.status(404).json({msg: `A post wit the ${id} was not found`})
-    }
-    else {
-        res.status(200).json(post)
-    }
-});
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
